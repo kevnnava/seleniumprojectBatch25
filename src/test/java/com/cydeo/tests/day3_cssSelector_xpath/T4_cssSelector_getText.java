f9995dd4495e420811bbc82d290c32c5f66885a4
kevinnava <kevin.nava00@gmail.com>
2022-02-17 01:31:50 -0600

    push 4

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T4_cssSelector_getText.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+public class T4_cssSelector_getText {
+
+    public static void main(String[] args) {
+
+        // TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
+        // 1- Open a Chrome browser
+        // 2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        // 3- Verify “Reset password” button text is as expected:
+        // Expected: Reset password
+        // locating reset password button using class attribute and its value
+        // locating reset password button using VALUE attribute and its value
+
+    }
+}
