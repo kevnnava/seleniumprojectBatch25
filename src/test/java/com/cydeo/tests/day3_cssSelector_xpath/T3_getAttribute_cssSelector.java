2995e2c690ba29dbe043507ca1e01759bb8d6b0d
kevinnava <kevin.nava00@gmail.com>
2022-02-17 00:57:19 -0600

    push 3

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T3_getAttribute_cssSelector.java
@@ -0,0 +1,18 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+public class T3_getAttribute_cssSelector {
+
+    public static void main(String[] args) {
+        // TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
+        // 1- Open a Chrome browser
+        // 2- Go to: https://login1.nextbasecrm.com/
+        // 3- Verify “Log in” button text is as expected:
+        // Expected: Log In
+        // Getting the value of the attribute "value"
+
+
+
+
+    }
+
+}
