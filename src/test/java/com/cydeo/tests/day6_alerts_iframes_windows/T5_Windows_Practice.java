47000e30bfed680184e68105b0de15995e6ccc2a
kevinnava <kevin.nava00@gmail.com>
2022-02-24 21:23:33 -0600

    day6 Task5 Multiple Windows

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,53 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        // 1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 2. Go to website: https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+        // Storing the main page's window handle as string is
+        // Good practice for future re-usable purposes
+        String mainHandle = driver.getWindowHandle();
+
+        System.out.println("mainHandle = " + mainHandle);
+
+        // 4. Assert: Title is “Windows”
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        // 5. Click to: “Click Here” link
+
+        // 6. Switch to new Window.
+
+        // 7. Assert: Title is “New Window”
+
+
+
+    }
+
+
+}
