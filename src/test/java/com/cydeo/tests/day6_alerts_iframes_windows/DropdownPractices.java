4b4bd9fc84ab895684e5508054df06959fdaecc2
kevinnava <kevin.nava00@gmail.com>
2022-02-24 18:03:18 -0600

    day6 Task6 date dropdowns

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,67 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        // 2. Go to https://practice.cydeo.com/dropdown
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+
+    @Test
+    public void dropdown_task5() throws InterruptedException {
+        // TC #5: Selecting state from State dropdown and verifying result 1. Open Chrome browse
+
+
+        // We located the dropdown and it is ready to use
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+        // Use all Select options. (visible, text, value, index)
+
+        // 3. Select Illinois
+        Thread.sleep(1000);
+        stateDropdown.selectByVisibleText("Illinois");
+
+        // 4. Select Virginia
+        Thread.sleep(1000);
+        stateDropdown.selectByValue("VA");
+
+        // 5. Select California
+        Thread.sleep(1000);
+        stateDropdown.selectByIndex(5);
+
+        // 6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
+
+    }
+
+    @Test
+    public void dropdown_task6(){
+        // Select “December 1 , 1923” and verify it is selected.
+        // Select year using : visible text
+        // Select month using : value attribute
+        // Select day using : index number
+        // creating expected values
+        // getting actual values from browser
+        // create assertions
+    }
+
+}
