3972d415014a07cdc64c5267e05180d46074ee7c
kevinnava <kevin.nava00@gmail.com>
2022-02-25 18:50:17 -0600

    day7 BrowserUtils

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -4,6 +4,11 @@ package com.cydeo.utilities;
 In this class only general utility methods that are not related to some specific page.
  */
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
 
     /*
@@ -21,7 +26,35 @@ public class BrowserUtils {
 
     }
 
-    public static void switchWindowAndVerify(){
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+
+        }
+
+        // 5. Assert: Title contains "expectedInTitle"
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+
+    }
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
 
     }
 
